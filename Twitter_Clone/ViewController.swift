5cd6a7d953b8c3fd8e907be9ebad177134712144
iyong-il <lyi1233313@gmail.com>
2022-10-28 06:59:55 +0900

    Initial Commit

---
+++ Twitter_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter_Clone
+//
+//  Created by 이용일(Rodi) on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
